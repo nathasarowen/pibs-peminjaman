9dfa2e2cf7978d51653d761b8b3e1b82f890ba46
Farrelmdk <farrelcornelius4@gmail.com>
2025-01-06 20:44:35 +0700

    Header: Membuat header untuk user

--- user/index.php
+++ user/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
@@ -11,7 +12,33 @@
         
         //buat crud nya
     ?>
-    <header></header>
+    <header>
+        <?php
+            require '../database.php';
+
+            //query buat ambil data site_settings dgn nama_setting "header"
+            $headerQuery = "SELECT nama_web, slogan_web, alamat FROM site_settings WHERE nama_setting = 'header'";
+            $headerResult = $conn->query($headerQuery);
+
+            //validasi jika query berhasil
+            if ($headerResult && $headerResult->num_rows > 0) {
+                $headerData = $headerResult->fetch_assoc();
+            } else {
+                $headerData = [
+                    'nama_web' => 'Nama Web Default',
+                    'slogan_web' => 'Slogan Web Default',
+                    'alamat' => 'Alamat Default'
+                ];
+            }
+            ?>
+
+            <img src="../aku.jpg" alt="Logo Web" class="logo">
+            <div class="header-info">
+                <h1><?= htmlspecialchars($headerData['nama_web'], ENT_QUOTES, 'UTF-8'); ?></h1>
+                <p style="font-weight: bold"><?= htmlspecialchars($headerData['slogan_web'], ENT_QUOTES, 'UTF-8'); ?></p>
+                <p><?= htmlspecialchars($headerData['alamat'], ENT_QUOTES, 'UTF-8'); ?></p>
+            </div>
+    </header>
     <nav></nav>
     <article></article>
     <aside></aside>
