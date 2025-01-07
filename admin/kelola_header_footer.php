0d0a41123b32e561a23dc5b7d8587a57a6bf06a9
nathasarowen <nathasa.abarua@gmail.com>
2025-01-07 22:55:08 +0700

    Menambahkan link font online

--- admin/kelola_header_footer.php
+++ admin/kelola_header_footer.php
@@ -47,6 +47,7 @@ $footerData = $footerResult ? $footerResult->fetch_assoc() : [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kelola Header & Footer</title>
     <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
--- user/index.php
+++ user/index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <script>
         function updateNamaPrasarana() {
             const select = document.getElementById('prasarana');
